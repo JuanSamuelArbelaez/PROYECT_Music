61ac52081656b7e44aacadf02c9f6d61fa99a1a7
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2023-04-24 15:50:17 -0500

    LinkedList and DoubleLinkedList added

---
+++ Model/Structures/DoubleLinkedList/DoubleLinkedList.py
@@ -0,0 +1,210 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.__size = 0
+        self.__sorted = False
+        self.__head = None
+
+    def size(self):
+        return self.__size
+
+    def is_sorted(self):
+        return self.__sorted
+
+    def is_empty(self):
+        return self.__size == 0
+
+    def contains(self, value):
+        node = self.__head
+        while node is not None:
+            if node.value == value:
+                return True
+            node = node.next
+        return False
+
+    def add(self, index, value):
+        if index < 0 or index > self.__size:
+            raise IndexError("Index out of range")
+
+        new_node = Node(value)
+        if index == 0:
+            new_node.next = self.__head
+            if self.__head is not None:
+                self.__head.prev = new_node
+            self.__head = new_node
+        else:
+            node = self.__head
+            for i in range(index - 1):
+                node = node.next
+            new_node.next = node.next
+            new_node.prev = node
+            if node.next is not None:
+                node.next.prev = new_node
+            node.next = new_node
+
+        self.__size += 1
+        self.__sorted = False
+
+    def append(self, value):
+        self.add(self.__size, value)
+
+    def remove(self, index):
+        if index < 0 or index >= self.__size:
+            raise IndexError("Index out of range")
+
+        if index == 0:
+            node = self.__head
+            self.__head = node.next
+            if self.__head is not None:
+                self.__head.prev = None
+        else:
+            node = self.__head
+            for i in range(index):
+                node = node.next
+            node.prev.next = node.next
+            if node.next is not None:
+                node.next.prev = node.prev
+
+        self.__size -= 1
+
+    def remove_value(self, value):
+        node = self.__head
+        index = 0
+        while node is not None:
+            if node.value == value:
+                self.remove(index)
+                return True
+            node = node.next
+            index += 1
+        return False
+
+    def poll(self, index):
+        if index < 0 or index >= self.__size:
+            raise IndexError("Index out of range")
+
+        node = self.__head
+        for i in range(index):
+            node = node.next
+
+        self.remove(index)
+
+        return node.value
+
+    def sort(self):
+        values = []
+        node = self.__head
+        while node is not None:
+            values.append(node.value)
+            node = node.next
+
+        values.sort()
+
+        self.clear()
+
+        for value in values:
+            self.append(value)
+
+        self.__sorted = True
+
+    def insert(self, value):
+        if not self.__sorted:
+            raise ValueError("List must be sorted to use insert method")
+
+        node = self.__head
+        index = 0
+        while node is not None:
+            if value <= node.value:
+                self.add(index, value)
+                return
+            node = node.next
+            index += 1
+
+        self.append(value)
+
+    def add_all(self, other_list):
+        for value in other_list:
+            self.append(value)
+
+        self.__sorted = False
+
+    def clear(self):
+        self.__head = None
+        self.__size = 0
+
+    def get(self, index):
+        if index < 0 or index >= self.__size:
+            raise IndexError("Index out of range")
+
+        node = self.__head
+        for i in range(index):
+            node = node.next
+
+        return node.value
+
+    def index_of(self, value):
+        node = self.__head
+        index = 0
+        while node is not None:
+            if node.value == value:
+                return index
+            node = node.next
+            index += 1
+        return -1
+
+    def last_index_of(self, value):
+        node = self.__head
+        index = 0
+        last_index = -1
+        while node is not None:
+            if node.value == value:
+                last_index = index
+            node = node.next
+            index += 1
+        return last_index
+
+    def valid_index(self, index):
+        return 0 <= index < self.__size
+
+    def sub_list(self, start, end):
+        if start < 0 or start >= self.__size:
+            raise IndexError("Start index out of range")
+
+        if end < 0 or end > self.__size:
+            raise IndexError("End index out of range")
+
+        if start > end:
+            raise ValueError("Start index cannot be greater than end index")
+
+        new_list = DoubleLinkedList()
+        node = self.__head
+        for i in range(start):
+            node = node.next
+        for i in range(start, end):
+            new_list.append(node.value)
+            node = node.next
+        return new_list
+
+    def remove_all(self, other_list):
+        node = other_list._head
+        while node is not None:
+            self.remove_value(node.value)
+            node = node.next
+
+    def append_list(self, other_list):
+        node = other_list._head
+        while node is not None:
+            self.append(node.value)
+            node = node.next
+
+    def iterator(self):
+        node = self.__head
+        while node is not None:
+            yield node.value
+            node = node.next
+
---
+++ Model/Structures/LinkedList/LinkedList.py
@@ -0,0 +1,201 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self._size = 0
+        self._sorted = False
+        self._head = None
+
+    def size(self):
+        return self._size
+
+    def sorted(self):
+        return self._sorted
+
+    def isEmpty(self):
+        return self._size == 0
+
+    def contains(self, value):
+        current = self._head
+        while current:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
+
+    def add(self, index, value):
+        if not self.validIndex(index):
+            raise IndexError("Invalid index")
+        if index == 0:
+            self._head = Node(value, self._head)
+        else:
+            current = self._head
+            for _ in range(index - 1):
+                current = current.next
+            current.next = Node(value, current.next)
+        self._size += 1
+        self._sorted = False
+
+    def append(self, value):
+        if self.isEmpty():
+            self._head = Node(value)
+        else:
+            current = self._head
+            while current.next:
+                current = current.next
+            current.next = Node(value)
+        self._size += 1
+        self._sorted = False
+
+    def remove(self, index):
+        if not self.validIndex(index):
+            raise IndexError("Invalid index")
+        if index == 0:
+            self._head = self._head.next
+        else:
+            current = self._head
+            for _ in range(index - 1):
+                current = current.next
+            current.next = current.next.next
+        self._size -= 1
+
+    def remove_by_value(self, value):
+        if self.isEmpty():
+            return
+        if self._head.value == value:
+            self._head = self._head.next
+            self._size -= 1
+            return
+        current = self._head
+        while current.next:
+            if current.next.value == value:
+                current.next = current.next.next
+                self._size -= 1
+                return
+            current = current.next
+
+    def poll(self, index):
+        if not self.validIndex(index):
+            raise IndexError("Invalid index")
+        current = self._head
+        for _ in range(index):
+            current = current.next
+        return current.value
+
+    def sort(self):
+        if self.isEmpty():
+            return
+        self._head = self._merge_sort(self._head)
+        self._sorted = True
+
+    def _merge_sort(self, head):
+        if not head or not head.next:
+            return head
+        middle = self._get_middle(head)
+        next_to_middle = middle.next
+        middle.next = None
+        left = self._merge_sort(head)
+        right = self._merge_sort(next_to_middle)
+        return self._sorted_merge(left, right)
+
+    def _sorted_merge(self, a, b):
+        if not a:
+            return b
+        if not b:
+            return a
+        if a.value <= b.value:
+            result = a
+            result.next = self._sorted_merge(a.next, b)
+        else:
+            result = b
+            result.next = self._sorted_merge(a, b.next)
+        return result
+
+    def _get_middle(self, head):
+        if not head:
+            return head
+        slow = head
+        fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def insert(self, value):
+        if not self._sorted:
+            raise ValueError("List is not sorted")
+        if self.isEmpty() or self._head.value >= value:
+            self._head = Node(value, self._head)
+            self._size += 1
+            return
+        current = self._head
+        while current.next and current.next.value < value:
+            current = current.next
+        current.next = Node(value, current.next)
+        self._size += 1
+
+    def addAll(self, other):
+        for value in other:
+            self.append(value)
+
+    def clear(self):
+        self._head = None
+        self._size = 0
+        self._sorted = False
+
+    def get(self, index):
+        return self.poll(index)
+
+    def indexOf(self, value):
+        index = 0
+        current = self._head
+        while current:
+            if current.value == value:
+                return index
+            current = current.next
+            index += 1
+        return -1
+
+    def lastIndexOf(self, value):
+        index = 0
+        last_index = -1
+        current = self._head
+        while current:
+            if current.value == value:
+                last_index = index
+            current = current.next
+            index += 1
+        return last_index
+
+    def validIndex(self, index):
+        return 0 <= index <= self._size
+
+    def subList(self, start, end):
+        if not self.validIndex(start) or not self.validIndex(end) or start > end:
+            raise IndexError("Invalid index")
+        sub_list = LinkedList()
+        current = self._head
+        for i in range(start):
+            current = current.next
+        for _ in range(end - start):
+            sub_list.append(current.value)
+            current = current.next
+        return sub_list
+
+    def removeAll(self, other):
+        for value in other:
+            while self.contains(value):
+                self.remove_by_value(value)
+
+    def appendList(self, other):
+        self.addAll(other)
+
+    def iterator(self):
+        current = self._head
+        while current:
+            yield current.value
+            current = current.next
